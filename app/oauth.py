a31c2a8b507f7890aa9e311a31ed8d67914ede81
Aleksandr Kurlov <sasha.kurlov@gmail.com>
2014-12-01 22:04:31 +0500

    Added Twitter

--- app/oauth.py
+++ app/oauth.py
@@ -1,5 +1,5 @@
 import config
-from flask import url_for, session, request
+from flask import url_for, session, request, redirect
 from rauth import OAuth2Service
 
 
@@ -64,7 +64,38 @@ class FacebookSignIn(OAuthSignIn):
         )
 
 class TwitterSignIn(OAuthSignIn):
-    pass
+    def __init__(self):
+        super(TwitterSignIn, self).__init__('twitter')
+        self.service = OAuth2Service(
+            name='twitter',
+            consumer_key=self.consumer_id,
+            consumer_secret=self.consumer_secret,
+            request_token_url='https://api.twitter.com/oauth/request_token',
+            authorize_url='https://api.twitter.com/oauth/authorize',
+            access_token_url='https://api.twitter.com/oauth/access_token',
+            base_url='https://api.twitter.com/1.1/'
+        )
+
+    def authorize(self):
+        request_token = self.service.get_request_token(
+            params={'oauth_callback': self.get_callback_url()}
+        )
+        session['request_token'] = request_token
+        return redirect(self.service.get_authorize_url(request_token[0]))
+
+    def callback(self):
+        request_token = session.pop('request_token')
+        if 'oauth_verifier' not in request.args:
+            return None, None, None
+        oauth_session = self.service.get_auth_session(
+            request_token[0],
+            request_token[1],
+            data={'oauth_verifier': request.args['oauth_verifier']}
+        )
+        me = oauth_session.get('account/verify_credentials.json').json()
+        social_id = 'twitter$' + str(me.get('id'))
+        username = me.get('screen_name')
+        return social_id, username, None   # Twitter does not provide email
 
 class VkontakteSignIn(OAuthSignIn):
     def __init__(self):
