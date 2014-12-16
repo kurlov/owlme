4801a9b6fbbfc481a988f316b32935409cdb0242
Aleksandr Kurlov <sasha.kurlov@gmail.com>
2014-12-17 03:08:08 +0500

    Added HTTP error handlers

--- app/views.py
+++ app/views.py
@@ -129,6 +129,15 @@ def contacts():
 def about_us():
     return render_template('about_us.html', title="About us")
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
+
 @oid.after_login
 def after_login(resp):
     if resp.email is None or resp.email == "":
