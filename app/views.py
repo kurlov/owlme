f8bf5176c883752fa1e389828e3d5f55cf4101ab
Aleksandr Kurlov <sasha.kurlov@gmail.com>
2014-12-18 22:35:34 +0500

    Added error template improves

--- app/views.py
+++ app/views.py
@@ -133,6 +133,10 @@ def about_us():
 def not_found_error(error):
     return render_template('404.html'), 404
 
+@app.route('/error')
+def smt():
+    return render_template('500.html')
+
 @app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
