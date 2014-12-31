1766303b434c63245fc1291e0c410e6da4566943
Aleksandr Kurlov <sasha.kurlov@gmail.com>
2014-12-31 19:38:52 +0500

    Deleted OpenID handler

--- app/views.py
+++ app/views.py
@@ -21,20 +21,6 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-@app.route('/login', methods=['GET', 'POST'])
-@oid.loginhandler
-def login():
-    if g.user is not None and g.user.is_authenticated():
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        session['remember_me'] = form.remember_me.data
-        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
-    return render_template('login.html',
-                           title='Sign In',
-                           form=form,
-                           providers=app.config['OPENID_PROVIDERS'])
-
 @app.route('/callback/<provider>')
 def oauth_callback(provider):
     if not current_user.is_anonymous():
