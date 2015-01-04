e8dca8bdec2f261d2b1e303139569d2791ef60c5
Aleksandr Kurlov <sasha.kurlov@gmail.com>
2015-01-04 23:02:44 +0500

    Deleted redundant stuff

--- config.py
+++ config.py
@@ -9,13 +9,6 @@ MAX_SEARCH_RESULTS = 50
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
-OPENID_PROVIDERS = [
-    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
-    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
-    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
-    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
-    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
-
 OAUTH_CREDENTIALS = {
     'facebook': {
         'id': '1578641885698624',
